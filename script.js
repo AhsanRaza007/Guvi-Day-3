cf42226c1dd75ba6d7379786183447221b4098e4
Md Ahsan Raza <ahsan007raza@gmail.com>
2020-11-10 03:51:26 +0530

    submission2

--- script.js
+++ script.js
@@ -6,20 +6,20 @@
 let API_KEY = "d4de9a989817001b17091776b492bea1";
 
 function getDataByCityName(city){
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
     fetch(url)
     .then(res=>res.json())
     .then(data=>console.log(data));
 }
 
 function getDataByCityId(city_id){
-    let url = `http://api.openweathermap.org/data/2.5/weather?id=${city_id}&appid=${API_KEY}`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?id=${city_id}&appid=${API_KEY}`;
     fetch(url)
     .then(res=>res.json())
     .then(data=>console.log(data));
 }
 function getDataByLatLon(lon, lat){
-    let url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
     fetch(url)
     .then(res=>res.json())
     .then(data=>console.log(data));
