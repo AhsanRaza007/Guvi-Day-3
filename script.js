58aee1d756a7d2b10c68e88d709d5f5c582d10b4
Md Ahsan Raza <ahsan007raza@gmail.com>
2020-11-10 03:53:03 +0530

    submission3

--- script.js
+++ script.js
@@ -9,20 +9,23 @@ function getDataByCityName(city){
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
     fetch(url)
     .then(res=>res.json())
-    .then(data=>console.log(data));
+    .then(data=>console.log(data))
+    .catch(err=>console.log(err));
 }
 
 function getDataByCityId(city_id){
     let url = `https://api.openweathermap.org/data/2.5/weather?id=${city_id}&appid=${API_KEY}`;
     fetch(url)
     .then(res=>res.json())
-    .then(data=>console.log(data));
+    .then(data=>console.log(data))
+    .catch(err=>console.log(err));
 }
 function getDataByLatLon(lon, lat){
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
     fetch(url)
     .then(res=>res.json())
-    .then(data=>console.log(data));
+    .then(data=>console.log(data))
+    .catch(err=>console.log(err));
 }
 
 getDataByCityName('kolkata');
